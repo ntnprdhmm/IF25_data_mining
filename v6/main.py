5f8dcd41ac1af406214251badb448b7b152744d5
prudywsh <antoineprudhomme5@gmail.com>
2017-06-17 12:54:28 +0200

    v6 : new ouput structure

--- v6/main.py
+++ v6/main.py
@@ -21,12 +21,11 @@ LABELS_FILENAME = 'twitter/collecte_manchester_labels.csv'
 CSV_SEPARATOR = ';'
 
 RESULTS_PATH = "results"
-K_VALUES = range(2, 5)
+K_VALUES = range(2, 4)
 COLORS = ['b', 'g', 'r', 'c', 'm', 'y', 'k'] * 10
 
 labels = read_csv_dataset(DATASETS_PATH + LABELS_FILENAME, CSV_SEPARATOR)
 labels_names = labels[0]
-labels_stats = labels[1]
 dataset = read_csv_dataset(DATASETS_PATH + DATA_FILENAME, CSV_SEPARATOR, len(labels_names))
 
 normalized_columns, normalizations_values, dataset = normalize(dataset)
@@ -34,28 +33,34 @@ normalized_columns, normalizations_values, dataset = normalize(dataset)
 # empty the /results folder
 empty_dir(RESULTS_PATH)
 
+nb_combinations = 0
+# create a folder for each attribut combination
+for i in range(len(labels_names)):
+    for j in range(len(labels_names)):
+        if i != j:
+            os.makedirs(RESULTS_PATH + '/' + labels_names[i] + '_' + labels_names[j])
+            nb_combinations = nb_combinations + 1
+
 for K in K_VALUES:
+
+    start_time = time.time()
 
     print("\n==========" + len(str(K))*"=" + "======")
     print("===== K : " + str(K) + " =====")
     print("==========" + len(str(K))*"=" + "======\n")
-
-    CLUSTER_RESULTS_PATH = RESULTS_PATH + "/k=" + str(K)
-    os.makedirs(CLUSTER_RESULTS_PATH)
 
     X = np.array(dataset, dtype=np.float_)
 
     clf = K_Means(K)
     clf.fit(X)
 
+    combinations_done = 0
     # for each attribut combination
     for i in range(len(labels_names)):
         for j in range(len(labels_names)):
             if i != j:
 
-                start_time = time.time()
-
-                print("\n" + labels_names[i] + " -- " + labels_names[j])
+                #print("\n" + labels_names[i] + " -- " + labels_names[j])
 
                 # loop through clusters
                 for classification in clf.classifications:
@@ -72,7 +77,10 @@ for K in K_VALUES:
                 # save the plotted graph
                 plt.xlabel(labels_names[i])
                 plt.ylabel(labels_names[j])
-                plt.savefig(CLUSTER_RESULTS_PATH + '/' + labels_names[i] + '_' + labels_names[j] + '.svg')
+                plt.savefig(RESULTS_PATH + '/' + labels_names[i] + '_' + labels_names[j] + '/k=' + str(classification + 1) + '.svg')
                 plt.clf()
 
-                print("[DONE] %s in seconds \n" % str(time.time() - start_time))
+                combinations_done = combinations_done + 1
+                print("DONE : " + str(combinations_done/nb_combinations))
+
+    print("[CLUSTER DONE] %s in seconds \n" % str(time.time() - start_time))
